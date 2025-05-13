7f78f7fc27a5aed47a105b4b6bf5107ab234a130
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-13 16:09:04 +0800

    - initialized project

---
+++ Alarm-Clock/Alarm_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alarm_ClockApp.swift
+//  Alarm-Clock
+//
+//  Created by Ronjie Diafante Man-on on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Alarm_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

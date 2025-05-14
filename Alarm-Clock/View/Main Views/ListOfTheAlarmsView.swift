9fda074a8be2ad7a88be8e36c71a8394fe5144ea
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 13:32:35 +0800

    - updated ListOfTheAlarmsView

--- Alarm-Clock/View/Main Views/ListOfTheAlarmsView.swift
+++ Alarm-Clock/View/Main Views/ListOfTheAlarmsView.swift
@@ -12,7 +12,7 @@ struct ListOfTheAlarmsView: View {
     
     @State var isActive = false
     @State var currentIndex: Int? = nil
-//    @State var addEditViewType: AddEditViewType = .standard
+    @State var addEditViewType: AddEditViewType = .standard
     
     var body: some View {
         NavigationStack {
@@ -20,21 +20,21 @@ struct ListOfTheAlarmsView: View {
                 VStack {
                     List {
                         ForEach(lnManager.alarmViewModels.indices, id: \.self) { i in
-//                            AlarmRowViewButton(
-//                                model: lnManager.alarmViewModels[i],
-//                                i: i,
-//                                currentIndex: $currentIndex,
-//                                isActive: $isActive
-//                            )
-//                            .padding(.vertical)
-                            
-                            
+                            AlarmRowViewButton(
+                                model: lnManager.alarmViewModels[i],
+                                i: i,
+                                currentIndex: $currentIndex,
+                                isActive: $isActive
+                            )
+                            .padding(.vertical)
                         }
                         .onDelete(perform: deleteMe)
                     }
-                    
-                    //                    SelectView(width: 50, addEditViewType: $addEditViewType, isActive: $isActive)
-                    
+                    SelectView(
+                        width: 50,
+                        addEditViewType: $addEditViewType,
+                        isActive: $isActive
+                    )
                 }
                 
                 FourCoolCircles()
@@ -44,45 +44,40 @@ struct ListOfTheAlarmsView: View {
             .navigationTitle("Alarm List")
             .sheet(
                 isPresented: $isActive,
-                onDismiss: {}) {
-                    //                    ChooseAlarmView(currentAlarmIndex: $currentIndex, addEditViewType: addEditViewType)
-                }
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        
-                        Button(action: {
-                            isActive.toggle()
-                        }, label: {
-                            Text("+")
-                                .font(.largeTitle)
-                                .fontWeight(.semibold)
-                                .foregroundColor(alarmBlack)
-                        })
-                    }
-                    
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        EditButton()
+                onDismiss: {}
+            ) {
+                ChooseAlarmView(currentAlarmIndex: $currentIndex, addEditViewType: addEditViewType)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button{
+                        isActive.toggle()
+                    } label: {
+                        Text("+")
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .foregroundColor(alarmBlack)
                     }
                 }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
         }
     }
     
     func deleteMe(offsets: IndexSet) {
-        // Remove from pending alarms
         for index in offsets {
             print("Remove request from \(lnManager.alarmViewModels[index].id)")
-            lnManager
-                .removeRequest(
-                    id: lnManager.alarmViewModels[index].id
-                )
-            
+            lnManager.removeRequest(id: lnManager.alarmViewModels[index].id)
         }
         
-        // Removing from alarmViewModels
         lnManager.alarmViewModels.remove(atOffsets: offsets)
-        
     }
 }
+
 #Preview {
     ListOfTheAlarmsView()
+        .environmentObject(LocalNotificationManager())
 }

a85ae016a57c5035542c77832d30b59d3acb3ebd
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 13:38:44 +0800

    - added TimeArcView

--- Alarm-Clock/View/Sub Views/TimeArcView.swift
+++ Alarm-Clock/View/Sub Views/TimeArcView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct TimeArcView: View {
+    let percentDifference: CGFloat
+    let strokeStyle: StrokeStyle
+    let size: CGFloat
+    let rotateCircleOffset: CGFloat
+    let color: Color
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .trim(from: 0, to: percentDifference)
+            .stroke(style: strokeStyle)
+            .foregroundColor(color)
+            .frame(width: size, height: size)
+            .rotationEffect(.degrees(-90))
+            .rotationEffect(.degrees(rotateCircleOffset))
     }
 }
 
 #Preview {
-    TimeArcView()
+    TimeArcView(
+        percentDifference: 0.5,
+        strokeStyle: StrokeStyle(lineWidth: 15, dash: [1, 2]),
+        size: 200,
+        rotateCircleOffset: 30,
+        color: alarmBlack)
 }

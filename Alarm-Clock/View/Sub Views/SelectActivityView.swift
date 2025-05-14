756f324964a8bd01a47e4f03c5929784671de2af
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 13:37:03 +0800

    - added SelectActivityView

--- Alarm-Clock/View/Sub Views/SelectActivityView.swift
+++ Alarm-Clock/View/Sub Views/SelectActivityView.swift
@@ -8,11 +8,66 @@
 import SwiftUI
 
 struct SelectActivityView: View {
+    @Binding var currentColorIndex: Int
+    @Binding var currentActivity: String
+    
+    var currentColor: Color {
+        mainColors[currentColorIndex]
+    }
+    
+    var nextIndex: Int {
+        (currentColorIndex + 1) % mainColors.count
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 10) {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(activities, id: \.self) { activity in
+                        let isSelectedActivity = activity == currentActivity
+                        Image(systemName: activity)
+                            .font(isSelectedActivity ? .title2 : .subheadline)
+                            .foregroundColor(
+                                isSelectedActivity ? currentColor : alarmNickel
+                            )
+                            .opacity(isSelectedActivity ? 1.0 : 0.7)
+                            .onTapGesture {
+                                withAnimation {
+                                    currentActivity = activity
+                                }
+                            }
+                    }
+                }
+            }
+            
+            Circle()
+                .fill(currentColor)
+                .frame(width: 20, height: 20)
+                .shadow(
+                    color: currentColor.opacity(0.7),
+                    radius: 10,
+                    x: 0,
+                    y: 5)
+                .overlay(Circle().stroke(lineWidth: 2))
+                .onTapGesture {
+                    withAnimation {
+                        currentColorIndex = nextIndex
+                    }
+                }
+        }
+        .padding(.horizontal, 5)
+        .padding(.vertical, 2)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(alarmNickel, lineWidth: 1)
+        )
     }
 }
 
 #Preview {
-    SelectActivityView()
+    SelectActivityView(
+        currentColorIndex: .constant(0),
+        currentActivity: .constant(activities[0])
+    )
+    .padding(.horizontal)
 }

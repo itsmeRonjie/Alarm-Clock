230662f0c3d553b18572fdec935303a70808d4bb
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 13:39:11 +0800

    - added ToBedWakeUpView

--- Alarm-Clock/View/Sub Views/ToBedWakeUpView.swift
+++ Alarm-Clock/View/Sub Views/ToBedWakeUpView.swift
@@ -8,11 +8,94 @@
 import SwiftUI
 
 struct ToBedWakeUpView: View {
+    let currentAlarmIndex: Int?
+    @State var alarmModel: AlarmModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                CancelSaveAlarm(
+                    currentAlarmIndex: currentAlarmIndex,
+                    alarmModel: $alarmModel
+                )
+                
+                AlarmToggleView(alarmEnabled: $alarmModel.alarmEnabled)
+                
+                Divider()
+                
+                HStack {
+                    Grid {
+                        GridRow {
+                            TimeOfDayIcon(date: alarmModel.start)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                GrayedTextView(text: "start")
+                                TimePicker(time: $alarmModel.start, scale: 1.3)
+                            }
+                        }
+                        
+                        GridRow {
+                            HStack {
+                                Divider()
+                                    .frame(height: 30)
+                                    .padding(2)
+                            }
+                        }
+                        
+                        GridRow {
+                            Image(systemName: alarmModel.activity)
+                                .foregroundColor(alarmModel.activityColor)
+                                .font(.headline)
+                            
+                            SelectActivityView(
+                                currentColorIndex: $alarmModel.colorIndex,
+                                currentActivity: $alarmModel.activity
+                            )
+                        }
+                        .padding(.vertical)
+                        
+                        GridRow {
+                            HStack {
+                                Divider()
+                                    .frame(height: 30)
+                                    .padding(2)
+                            }
+                        }
+                        
+                        GridRow {
+                            TimeOfDayIcon(date: alarmModel.end)
+                                .font(.largeTitle)
+                            VStack(alignment: .leading) {
+                                TimePicker(time: $alarmModel.end, scale: 1.3)
+                                GrayedTextView(text: "end")
+                            }
+                        }
+                        
+                        GridRow {
+                            Text("")
+                            
+                            SoundMenuViewFromButton(alarmModel: $alarmModel)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .padding()
+            }
+            .padding()
+            .background(alarmCardBackgroundColor.cornerRadius(20))
+            .padding()
+        }
     }
 }
 
 #Preview {
-    ToBedWakeUpView()
+    ZStack {
+        Color
+            .gray
+            .opacity(0.3)
+            .ignoresSafeArea()
+        
+        ToBedWakeUpView(currentAlarmIndex: nil, alarmModel: .DefaultAlarm())
+    }
 }

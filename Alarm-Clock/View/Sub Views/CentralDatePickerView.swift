e7f2cc2d687778a801344ba403e5ef010188b100
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 13:34:39 +0800

    - added CentrakDatePickerView

--- Alarm-Clock/View/Sub Views/CentralDatePickerView.swift
+++ Alarm-Clock/View/Sub Views/CentralDatePickerView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct CentralDatePickerView: View {
+    let size: CGFloat
+    @Binding var alarmModel: AlarmModel
+    
+    var lineWidth: CGFloat = 10.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .stroke(alarmLightGray, lineWidth: lineWidth)
+            .frame(width: size, height: size)
+            .overlay(
+                VStack {
+                    VStack {
+                        GrayedTextView(
+                            text: "start",
+                            font: .caption)
+                        TimePicker(time: $alarmModel.start.animation(), scale: 1)
+                    }
+                    
+                    Spacer()
+                    
+                    GrayedTextView(text: "Set the alarm")
+                    
+                    Spacer()
+                    
+                    VStack {
+                        TimePicker(time: $alarmModel.end.animation(), scale: 1)
+                        GrayedTextView(
+                            text: "end",
+                            font: .caption)
+                    }
+                }
+                .padding()
+                .padding(.vertical)
+            )
     }
 }
 
 #Preview {
-    CentralDatePickerView()
+    CentralDatePickerView(
+        size: 200,
+        alarmModel: .constant(AlarmModel.DefaultAlarm())
+    )
 }

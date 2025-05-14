c8eb12979800c61c7cfba331dcb9e2ccef9a1102
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 13:35:16 +0800

    - added CircularTimeView

--- Alarm-Clock/View/Sub Views/CircularTimeView.swift
+++ Alarm-Clock/View/Sub Views/CircularTimeView.swift
@@ -8,11 +8,86 @@
 import SwiftUI
 
 struct CircularTimeView: View {
+    let currentAlarmIndex: Int?
+    let size: CGFloat
+    @Binding var alarmModel: AlarmModel
+
+    var startTime: Date {
+        alarmModel.start
+    }
+    
+    var endTime: Date {
+        alarmModel.end
+    }
+    
+    var percentDifference: CGFloat {
+        let value = dateToPercent(date: endTime) - dateToPercent(date: startTime)
+        return value >= 0 ? value : 1 + value
+    }
+    
+    var startDateToPercent: CGFloat {
+        dateToPercent(date: startTime)
+    }
+    
+    var endDateToPercent: CGFloat {
+        startDateToPercent + percentDifference
+    }
+    
+    var rotateCircleOffset: CGFloat {
+        360 * startDateToPercent
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            CentralDatePickerView(
+                size: size,
+                alarmModel: $alarmModel
+            )
+            
+            TimeArcView(
+                percentDifference: percentDifference,
+                strokeStyle: StrokeStyle(
+                    lineWidth: 20,
+                    lineCap: .round,
+                    lineJoin: .round
+                ),
+                size: size,
+                rotateCircleOffset: rotateCircleOffset,
+                color: .black
+            )
+            
+            TimeArcView(
+                percentDifference: percentDifference,
+                strokeStyle: StrokeStyle(
+                    lineWidth: 15,
+                    dash: [1,2]
+                ),
+                size: size,
+                rotateCircleOffset: rotateCircleOffset,
+                color: .gray
+            )
+            
+            DisplayIconOnCircularTimeView(
+                time: startTime,
+                size: size,
+                percent: startDateToPercent
+            )
+            
+            DisplayIconOnCircularTimeView(
+                time: endTime,
+                size: size,
+                percent: endDateToPercent
+            )
+        }
     }
 }
 
 #Preview {
-    CircularTimeView()
+    VStack(spacing: 50) {
+        CircularTimeView(
+            currentAlarmIndex: nil,
+            size: screenWidth / 2,
+            alarmModel: .constant(.DefaultAlarm())
+        )
+    }
 }

06bd81d2d80e0edf94865324b82a1dcf21be41f0
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 13:38:01 +0800

    - added SoundMenuViewFromButton

--- Alarm-Clock/View/Sub Views/SoundMenuViewFromButton.swift
+++ Alarm-Clock/View/Sub Views/SoundMenuViewFromButton.swift
@@ -8,11 +8,84 @@
 import SwiftUI
 
 struct SoundMenuViewFromButton: View {
+    @Binding var alarmModel: AlarmModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationLink {
+            SoundMenuView(alarmSound: $alarmModel.sound)
+        } label: {
+            HStack {
+                Text("Sound")
+                    .fontWeight(.semibold)
+                Text(alarmModel
+                    .sound
+                    .rawValue
+                    .formatSoundName
+                )
+                .font(.caption)
+                .fontWeight(.thin)
+            }
+            .padding(7)
+            .overlay(Capsule().stroke())
+        }
+    }
+}
+
+struct SoundMenuView: View {
+    @Binding var alarmSound: Sounds
+    var body: some View {
+        Form {
+            Section(header: Text("Ringtone Sounds")) {
+                ForEach(ringToneSoundsList, id: \.self) { sound in
+                    Button {
+                        alarmSound = sound
+                        playSound(sound: sound.rawValue)
+                    } label: {
+                        HStack {
+                            Image(systemName:  "checkmark")
+                                .foregroundColor(alarmPurple)
+                                .fontWeight(.semibold)
+                                .opacity(alarmSound == sound ? 1.0 : 0.0)
+                            
+                            Text(sound.rawValue.formatSoundName)
+                                .foregroundColor(alarmBlack)
+                                .fontWeight(.semibold)
+                        }
+                    }
+                }
+            }
+            
+            Section(header: Text("Sounds of Nature")) {
+                ForEach(natureSoundsList, id: \.self) { sound in
+                    Button {
+                        alarmSound = sound
+                        playSound(sound: sound.rawValue)
+                    } label: {
+                        HStack {
+                            Image(systemName:  "checkmark")
+                                .foregroundColor(alarmPurple)
+                                .fontWeight(.semibold)
+                                .opacity(alarmSound == sound ? 1.0 : 0.0)
+                            
+                            Text(sound.rawValue.formatSoundName)
+                                .foregroundColor(alarmBlack)
+                                .fontWeight(.semibold)
+                        }
+                    }
+                }
+            }
+        }
+        .onDisappear {
+            stopPlaying()
+        }
     }
 }
 
 #Preview {
-    SoundMenuViewFromButton()
+    NavigationStack {
+        SoundMenuViewFromButton(
+            alarmModel: .constant(AlarmModel.DefaultAlarm())
+        )
+    }
 }

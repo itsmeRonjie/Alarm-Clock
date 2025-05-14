17ef356e52104e3c08ab2c7a6f642719567292e7
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 11:30:49 +0800

    - added Image Constatn

--- Alarm-Clock/Constants/Image.swift
+++ Alarm-Clock/Constants/Image.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+let welcome = "welcome"
+let appImageSamples = "AppImageSamples"
+let partyPerson = "undraw_Beach_day_cser"
+let circularViewImage = "CircularViewImage"
+let standardViewImage = "StandardViewImage"

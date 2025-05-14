5537b745d5afac86bc353845854e77551b6b2112
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 13:35:37 +0800

    - added DisplayIconOnCircularTimeView

--- Alarm-Clock/View/Sub Views/DisplayIconOnCircularTimeView.swift
+++ Alarm-Clock/View/Sub Views/DisplayIconOnCircularTimeView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct DisplayIconOnCircularTimeView: View {
+    let time: Date
+    let size: CGFloat
+    let percent: CGFloat
+    var circleFrame = 40.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .fill(alarmCardBackgroundColor)
+            .frame(width: circleFrame, height: circleFrame)
+            .shadow(color: alarmBlack.opacity(0.5), radius: 1, x: 0, y: 1)
+            .overlay(TimeOfDayIcon(date: time))
+            .rotation3DEffect(
+                .degrees(-percent*360),
+                axis: (x: 0, y: 0, z: 1)
+            )
+            .offset(y: -size / 2)
+            .rotation3DEffect(
+                .degrees(percent*360),
+                axis: (x: 0, y: 0, z: 1)
+            )
     }
 }
 
 #Preview {
-    DisplayIconOnCircularTimeView()
+    ZStack {
+        alarmBlack.ignoresSafeArea()
+        DisplayIconOnCircularTimeView(
+            time: Date(),
+            size: 200,
+            percent: 0.5)
+    }
 }

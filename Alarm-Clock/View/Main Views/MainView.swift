4d34a30eff7edadeb4941c663be7ac739cde9d4c
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 11:36:25 +0800

    - updated Main View

--- Alarm-Clock/View/Main Views/MainView.swift
+++ Alarm-Clock/View/Main Views/MainView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct MainView: View {
+    @EnvironmentObject var lnManager: LocalNotificationManager
+    
+    @Environment(\.scenePhase) var scenePhase
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            if lnManager.isAuthorized {
+                Text("ListOfTheAlarmsView")
+                    .tabItem({
+                        Label("Alarms", systemImage: "alarm.fill")
+                    })
+                
+                Text("AboutView")
+                    .tabItem({
+                        Label("About", systemImage: "info.circle.fill")
+                    })
+            } else {
+//                EnableNotifications()
+            }
+            
         }
-        .padding()
+        .ignoresSafeArea()
+        .task {
+            try? await lnManager
+                .requestAuthorization()
+        }
+        .onChange(of: scenePhase) { _, newValue in
+            if newValue == .active {
+                Task {
+                    await lnManager
+                        .getCurrentSettings()
+                    
+                    await lnManager
+                        .getPendingAlarms()
+                }
+            }
+        }
     }
 }
+
 
 #Preview {
     MainView()

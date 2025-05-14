b82a9665be8c25b9179bd3a41dc08b6622141954
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 11:54:26 +0800

    - added ButtonView

--- Alarm-Clock/View/Sub Views/ButtonView.swift
+++ Alarm-Clock/View/Sub Views/ButtonView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct ButtonView: View {
+    let text: LocalizedStringKey
+    var endRadius = 40.0
+    var scaleX = 3.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(text)
+            .foregroundColor(alarmBlack)
+            .fontWeight(.semibold)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(
+                MainGradient(
+                    endRadius: endRadius,
+                    scaleX: scaleX
+                )
+                .cornerRadius(30)
+            )
     }
 }
 
+
 #Preview {
-    ButtonView()
+    VStack {
+        ButtonView(text: "add alarm")
+        ButtonView(text: "next")
+        ButtonView(text: "create")
+    }
+    .padding()
 }

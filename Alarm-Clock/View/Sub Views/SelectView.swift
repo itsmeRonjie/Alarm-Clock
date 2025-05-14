22931a33c4827ae04196206d0274569b99f783c5
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-14 13:37:37 +0800

    - added SelectView

--- Alarm-Clock/View/Sub Views/SelectView.swift
+++ Alarm-Clock/View/Sub Views/SelectView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct SelectView: View {
+    let width: CGFloat
+    @Binding var addEditViewType: AddEditViewType
+    @Binding var isActive: Bool
+    
+    var isStandardView: Bool {
+        addEditViewType == .standard
+    }
+    
+    var isCircularView: Bool {
+        addEditViewType == .circular
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button{
+                withAnimation {
+                    addEditViewType = .standard
+                    isActive.toggle()
+                }
+            } label: {
+                ImageDisplayView(name: standardViewImage, width: 50)
+                    .scaleEffect(isStandardView ? 1.05 : 1.0)
+                    .shadow(color: .blue, radius: isStandardView ? 5 : 0, x: 0, y: 0)
+            }
+            
+            Spacer()
+            
+            Button {
+                withAnimation {
+                    addEditViewType = .circular
+                    isActive.toggle()
+                }
+            } label: {
+                ImageDisplayView(name: circularViewImage, width: 50)
+                    .scaleEffect(isCircularView ? 1.05 : 1.0)
+                    .shadow(color: .blue, radius: isCircularView ? 5 : 0, x: 0, y: 0)
+            }
+        }
+        .padding()
     }
 }
 
 #Preview {
-    SelectView()
+    VStack {
+        SelectView(width: 50, addEditViewType: .constant(.standard), isActive: .constant(true))
+        SelectView(width: 50, addEditViewType: .constant(.circular), isActive: .constant(true))
+    }
 }
